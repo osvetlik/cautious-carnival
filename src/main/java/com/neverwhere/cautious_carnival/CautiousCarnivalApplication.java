d533cddf20ad4d6116639d9ed7307627801bb37f
Ondřej Světlík <ondrej.svetlik@yoso.fi>
2026-07-08 14:29:18 +0200

    Generated Spring Boot project template

---
+++ src/main/java/com/neverwhere/cautious_carnival/CautiousCarnivalApplication.java
@@ -0,0 +1,13 @@
+package com.neverwhere.cautious_carnival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CautiousCarnivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CautiousCarnivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neverwhere/cautious_carnival/CautiousCarnivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neverwhere.cautious_carnival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CautiousCarnivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
